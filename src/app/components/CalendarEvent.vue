0417dcceed1e88bbac18aaa1557ddd577d4b80a8
TonyKioko <tonykioko384@gmail.com>
2020-02-19 10:57:53 +0300

    can delete events

--- src/app/components/CalendarEvent.vue
+++ src/app/components/CalendarEvent.vue
@@ -1,52 +1,55 @@
 <template>
   <div class="day-event" :style="getEventBackgroundColor">
     <div v-if="!event.edit">
-      <span class="has-text-centered details">{{ event.details }}</span> 
+      <span class="has-text-centered details">{{ event.details }}</span>
       <div class="has-text-centered icons">
-        <i class="fa fa-pencil-square edit-icon" 
-          @click="editEvent(day.id, event.details)"></i> 
-        <i class="fa fa-trash-o delete-icon"></i>
+        <i class="fa fa-pencil-square edit-icon" @click="editEvent(day.id, event.details)"></i>
+        <i class="fa fa-trash-o delete-icon" @click="deleteEvent(day.id, event.details)"></i>
       </div>
     </div>
     <div v-if="event.edit">
-      <input type="text" :placeholder="event.details" v-model="newEventDetails"/>
+      <input type="text" :placeholder="event.details" v-model="newEventDetails" />
       <div class="has-text-centered icons">
-        <i class="fa fa-check" 
-          @click="updateEvent(day.id, event.details, newEventDetails)"></i>
+        <i class="fa fa-check" @click="updateEvent(day.id, event.details, newEventDetails)"></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { store } from '../store.js';
+import { store } from "../store.js";
 
 export default {
-  name: 'CalendarEvent',
-  props: ['event', 'day'],
-  data () {
+  name: "CalendarEvent",
+  props: ["event", "day"],
+  data() {
     return {
-      newEventDetails: ''
-    }
+      newEventDetails: ""
+    };
   },
   computed: {
     getEventBackgroundColor() {
-      const colors = ['#FF9999', '#85D6FF', '#99FF99'];
+      const colors = ["#FF9999", "#85D6FF", "#99FF99"];
       let randomColor = colors[Math.floor(Math.random() * colors.length)];
       return `background-color: ${randomColor}`;
     }
   },
   methods: {
-    editEvent (dayId, eventDetails) {
+    editEvent(dayId, eventDetails) {
       store.editEvent(dayId, eventDetails);
     },
-    updateEvent (dayId, originalEventDetails, updatedEventDetails) {
-      if (updatedEventDetails === '') updatedEventDetails = originalEventDetails;
+    updateEvent(dayId, originalEventDetails, updatedEventDetails) {
+      if (updatedEventDetails === "")
+        updatedEventDetails = originalEventDetails;
       store.updateEvent(dayId, originalEventDetails, updatedEventDetails);
-      this.newEventDetails = '';
+      this.newEventDetails = "";
+    },
+    deleteEvent(dayId, eventDetails) {
+      console.log("delete clicked");
+      store.deleteEvent(dayId, eventDetails);
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -54,7 +57,7 @@ export default {
   margin-top: 6px;
   margin-bottom: 6px;
   display: block;
-  color: #4C4C4C;
+  color: #4c4c4c;
   padding: 5px;
 
   .details {
@@ -68,7 +71,7 @@ export default {
   input {
     background: none;
     border: 0;
-    border-bottom: 1px solid #FFF;
+    border-bottom: 1px solid #fff;
     width: 100%;
 
     &:focus {
