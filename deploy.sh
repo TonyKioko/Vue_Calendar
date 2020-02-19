eb7266d9f4c29e38eb5d8c323558f91629d329b7
TonyKioko <tonykioko384@gmail.com>
2020-02-19 12:13:48 +0300

    preparing to deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:TonyKioko/Vue_Calendar.git master:gh-pages
+
+cd -
