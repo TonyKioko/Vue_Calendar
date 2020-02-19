433b02b6eefc61ba337b104f5c131013c3d5348f
TonyKioko <tonykioko384@gmail.com>
2020-02-19 10:25:06 +0300

    submit event functionality & error handling added

--- src/app/components/CalendarEntry.vue
+++ src/app/components/CalendarEntry.vue
@@ -1,31 +1,47 @@
 <template>
   <div id="calendar-entry">
     <div class="calendar-entry-note">
-      <input type="text" placeholder="New Event" />
+      <input type="text" placeholder="New Event" v-model="inputEntry" required />
       <p class="calendar-entry-day">
-        Day of event: <span class="bold">{{ titleOfActiveDay }}</span>
+        Day of event:
+        <span class="bold">{{ titleOfActiveDay }}</span>
       </p>
-      <a class="button is-primary is-small is-outlined">Submit</a>
+      <a class="button is-primary is-small is-outlined" @click="submitEvent(inputEntry)">Submit</a>
     </div>
+    <p style="color: red; font-size: 13px" v-if="error">You must type something first!</p>
   </div>
 </template>
 
 <script>
-import { store } from '../store.js';
+import { store } from "../store.js";
 
 export default {
-  name: 'CalendarEntry',
+  name: "CalendarEntry",
   computed: {
-    titleOfActiveDay () {
+    titleOfActiveDay() {
       return store.getActiveDay().fullTitle;
     }
   },
-}
+  data() {
+    return {
+      inputEntry: "",
+      error: false
+    };
+  },
+  methods: {
+    submitEvent(eventDetails) {
+      if (eventDetails === "") return (this.error = true);
+      store.submitEvent(eventDetails);
+      this.inputEntry = "";
+      this.error = false;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 #calendar-entry {
-  background: #FFF;
+  background: #fff;
   border: 1px solid #42b883;
   border-radius: 10px;
   max-width: 300px;
@@ -37,7 +53,7 @@ export default {
       width: 200px;
       font-weight: 600;
       border: 0;
-      border-bottom: 1px solid #CCC;
+      border-bottom: 1px solid #ccc;
       font-size: 15px;
       height: 30px;
       margin-bottom: 10px;
